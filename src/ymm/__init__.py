7a680428dfd734bb03663134557ab934634eefba
drernie <ernest@nauto.com>
2022-03-04 06:07:00 -0800

    from_file/run_file

--- src/ymm/__init__.py
+++ src/ymm/__init__.py
@@ -1,9 +1,21 @@
 # ymm v0.1
-# Compact Query Meta-language
-# https://github.com/TheSwanFactory/hclang/blob/master/hc/ymm.hc
+# YAML Mimics Makefiles
 
-# TODO: https://github.com/LucaCanali/sparkMeasure
+DEFAULT_FILE="ymm.yml"
 
-#from pyspark.sql.types import DoubleType,IntegerType,LongType
-from .cvm import CVM
-from .wrappers import *
+class YMM:
+    def __init__(self, yaml):
+        self.yaml = yaml
+
+    def run(self):
+        return True
+
+def from_file(yaml_file):
+    print("Loading "+yaml_file)
+    with open(yaml_file) as data:
+        raw_yaml = yaml.full_load(data)
+        return YMM(raw_yaml)
+
+def run_file(yaml_file=DEFAULT_FILE):
+    ymm = from_file(name, spark, folder)
+    return ymm.run()

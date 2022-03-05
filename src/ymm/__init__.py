546e219b93a0e0c011c6196d177ee08199b7439e
Dr. Ernie Prabhakar <ernest@nauto.com>
2022-03-05 08:33:09 -0800

    Ymm load v0.1.0 (#1)
    * copy from cqml
    
    * stub test
    
    * from_file/run_file
    
    * split into files
    
    * import context
    
    * pass test_load
    
    * pass test_run
    
    * stub cli
    
    * top-level ymm
    
    * add_argument('-f','--file
    
    * load_file(file)
    
    * print better
    
    * remove print
    
    * self, cmd
    
    * release --no-input
    
    * Back to development: 0.1.1

--- src/ymm/__init__.py
+++ src/ymm/__init__.py
@@ -1,9 +1,5 @@
 # ymm v0.1
-# Compact Query Meta-language
-# https://github.com/TheSwanFactory/hclang/blob/master/hc/ymm.hc
+# YAML Mimics Makefiles
 
-# TODO: https://github.com/LucaCanali/sparkMeasure
-
-#from pyspark.sql.types import DoubleType,IntegerType,LongType
-from .cvm import CVM
-from .wrappers import *
+from .ymm import YMM
+from .file import *

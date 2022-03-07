7115f89a10b3eab0a904f5683073d36ce854c820
drernie <ernest@nauto.com>
2022-03-07 11:46:06 -0800

    key = f'{self.arg}.{self.i}'

--- src/ymm/ymm.py
+++ src/ymm/ymm.py
@@ -6,10 +6,13 @@ class YMM:
     def __init__(self, yaml, debug=True):
         self.yaml = yaml
         self.env = {}
+        self.i = 0
 
     def run(self,arg=False):
-        if not arg in self.yaml: sys.exit('ERROR: action [{arg}] not found')
+        if not arg in self.yaml: sys.exit(f'ERROR: action [{arg}] not found')
         actions = self.yaml[arg]
+        self.i = 0
+        self.arg = arg
         results = [self.execute(cmd) for cmd in actions]
         return results
 
@@ -20,9 +23,16 @@ class YMM:
         self.log(args)
         result = subprocess.run(args, stdout=subprocess.PIPE)
         msg = result.stdout.decode("utf-8").strip()
+        self.save(msg)
+        return msg
+
+    def save(self, msg):
         print(f'# {msg}')
+        self.i += 1
+        key = f'{self.arg}.{self.i}'
         if kLast in self.env: self.env[kPrior] = self.env[kLast]
         self.env[kLast]=msg
+        self.env[key]=msg
         return msg
 
     def log(self, arg):

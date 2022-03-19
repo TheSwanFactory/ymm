ebfebb2502f306534a22bedc7e1088db57b94485
drernie <ernest@nauto.com>
2022-03-18 17:42:56 -0700

    class Args

--- tests/context.py
+++ tests/context.py
@@ -1,6 +1,14 @@
 import os
 import sys
+import ymm
+from dataclasses import dataclass
+
+TEST_FILE="tests/ymm.yml"
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '.','./src','..','../src')))
 
-import ymm
-TEST_FILE="tests/ymm.yml"
+@dataclass
+class Args:
+    actions: list[str]
+    file: str = TEST_FILE
+    list: bool = False
+    no_init: bool = False
